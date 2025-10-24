06933932081604632b5a8264b3b41c126cc99854
fourhtyoz <fourhtyoz@gmail.com>
2025-10-24 23:11:06 +0300

    parse currency

--- money/decimal.go
+++ money/decimal.go
@@ -1,6 +1,44 @@
 package money
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	ErrInvalidDecimal = Error("unable to convert the decimal")
+	ErrTooLarge = Error("quantity over 10ˆ12 is too large")
+)
+
 // Decimal is capable of storing a floating-point value.
 type Decimal struct {
+	subunits int64
+	precision byte
+}
 
+func ParseDecimal(amount string) (Decimal, error) {
+	intPart, fracPart, _ := strings.Cut(amount, ".")
+
+	const maxDecimal = 12
+
+	if len(intPart) > maxDecimal {
+		return Decimal{}, ErrTooLarge
+	}
+
+	subunits, err := strconv.ParseInt(intPart+fracPart, 10, 64)
+	if err != nil {
+		return Decimal{}, fmt.Errorf("%w: %s", ErrInvalidDecimal, err)
+	}
+
+	precision := byte(len(fracPart))
+
+	return Decimal{subunits: subunits, precision: precision}, nil
+}
+
+func (d *Decimal) simplify() {
+	for d.subunits % 10 == 0 && d.precision > 0 {
+		d.precision--
+		d.subunits /= 10
+	}
 }
